8eafd639640a870dc3cdc725256e3967aeeb2e52
foveejong <jovefong62@gmail.com>
2024-06-15 22:12:53 +0800

    Add Babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,50 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    devtool: "inline-source-map",
-    entry: "./src/index.js",
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "./dist"),
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist'),
         clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: "Restaurant Page",
-            filename: "index.html",
-            template: "./src/index.html",
-            inject: "head",
-            scriptLoading: "defer"
+            title: 'Restaurant Page',
+            filename: 'index.html',
+            template: './src/index.html',
+            inject: 'head',
+            scriptLoading: 'defer',
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
+                use: ['style-loader', 'css-loader'],
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
+                type: 'asset/resource',
             },
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource'
-            }
-        ]
-    }
-}
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                        ],
+                        plugins: ['@babel/plugin-proposal-class-properties'],
+                    },
+                },
+            },
+        ],
+    },
+};
